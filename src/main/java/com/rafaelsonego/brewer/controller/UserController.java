95dc428f19beb17e4f16f7fbc4c3f7e1c25fafad
Rafael Silva <rafael2785@gmail.com>
2018-06-11 21:41:09 -0400

    Create new page and controller for users

---
+++ src/main/java/com/rafaelsonego/brewer/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.rafaelsonego.brewer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping("/user/new")
+	public String newUser() {
+		return "beer/new-user";
+	}
+
+}
